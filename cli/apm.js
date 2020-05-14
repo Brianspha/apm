95735ddfcc54cc9c55eb5219567c188af6820c81
Siphamandla brian Mjoli <g14m1190@campus.ru.ac.za>
2020-05-14 22:04:51 +0200

    init

---
+++ cli/apm.js
@@ -0,0 +1,8 @@
+'use strict';
+ 
+var ArgumentParser = require('../lib/argparse').ArgumentParser;
+var parser = new ArgumentParser({
+  version: '0.0.1',
+  addHelp:true,
+  description: 'Arvweave Package Manager'
+});
